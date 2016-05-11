8e7c6b73f2737cbd11817653ded36a1df1ade15d
Jonathan Halverson <halverson.jonathan@gmail.com>
2016-05-11 19:48:20 -0400

    Examples of using Spark SQL

--- spark/intro_map_filter.py
+++ spark/intro_map_filter.py
@@ -2,6 +2,9 @@
    Friday, April 29, 2016
 
    This is a simple PySpark script to illustrate the basics.
+
+   Usage: ~/software/spark-1.6.1-bin-hadoop2.6/bin/spark-submit --master local[*] --name "Simple map" intro_map_filter.py
+   UI: http://localhost:4040
 """
 
 from pyspark import SparkContext
@@ -22,3 +25,5 @@ print numRDD.top(10)
 # here we create the RDD locally
 myRDD = sc.parallelize(range(10000))
 print myRDD.stdev()
+
+sc.stop()
