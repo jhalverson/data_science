147686b1f14d8ddc66c5375e63fc44636dc7f0b7
Jonathan Halverson <halverson.jonathan@gmail.com>
2016-05-24 16:42:06 -0400

    Updated restaurants project

--- project_boston_restaurants/helper_methods.py
+++ project_boston_restaurants/helper_methods.py
@@ -1,6 +1,6 @@
 def biz2yelp():
     import pandas as pd
-    df = pd.read_csv('restaurant_ids_to_yelp_ids.csv')
+    df = pd.read_csv('data/restaurant_ids_to_yelp_ids.csv')
     d0 = df[['restaurant_id', 'yelp_id_0']]
     d1 = df[['restaurant_id', 'yelp_id_1']][df.yelp_id_1.notnull()]
     d1.rename(columns={'yelp_id_1': 'yelp_id_0'}, inplace=True)
@@ -17,3 +17,15 @@ def read_json(filename):
     data = map(lambda x: x.rstrip(), data)
     data_json_str = "[" + ','.join(data) + "]"
     return pd.read_json(data_json_str)
+
+def drop_duplicate_inspections(df, threshold=60):
+    """Removes duplicate inspections."""
+    import numpy as np
+    indices_to_drop = []
+    for i in range(0, df.shape[0]):
+        if (i + 1 == df.shape[0]): break
+        restaurant_match = df.iloc[i, 2] == df.iloc[i + 1, 2]
+        violation_match = all([df.iloc[i, j] == df.iloc[i + 1, j] for j in [3, 4, 5]])
+        date_match = (df.iloc[i + 1, 1] - df.iloc[i, 1]) / np.timedelta64(1, 'D') < threshold
+        if (restaurant_match and violation_match and date_match): indices_to_drop.append(df.index[i + 1])
+    return df.drop(indices_to_drop)
