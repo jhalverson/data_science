825f4344ba3f152bf21dc6fbdead80d66fbcded9
Jonathan Halverson <halverson.jonathan@gmail.com>
2017-01-19 18:08:20 -0500

    Comparison of scraping an HTML table: Pandas vs BS

--- hadoop/top_directors.py
+++ hadoop/top_directors.py
@@ -2,32 +2,63 @@
 # Wednesday, January 18, 2017
 # Scraping the-numbers.com data
 
+from __future__ import print_function
+
+# the webpage containing the data
+url = 'http://www.the-numbers.com/people/worldwide-records/technical-roles/Director'
+
+############################
+## BeautifulSoup Approach ##
+############################
+
 import requests
 from bs4 import BeautifulSoup
 
-# download the HTML page
-url = 'http://www.the-numbers.com/people/worldwide-records/technical-roles/Director'
+# download the HTML page and construct the parse tree
 r = requests.get(url)
-
-# construct a parse tree using BeautifulSoup
 s = BeautifulSoup(r.content, 'lxml')
 
-#print len(s.find_all('table'))
-#print s.find_all('table')
-sct = s.find_all('table')
+# extract the one and only table
+tables = s.find_all('table')
+table = tables[0]
 
-print sct
-for item in sct.find_all('td', {'class':'data'}):
-  print item.text
+# extract the data from each row
+rows = []
+for row in table.find_all('tr'):
+  rows.append([data.text for data in row.find_all('td') + row.find_all('th')])
 
-import sys; sys.exit(0)
+# convert list of lists to a Pandas dataframe
+import pandas as pd
+df = pd.DataFrame(rows[1:], columns=rows[0])
 
+# add new columns in numeric data types
+df['ave_num'] = df['Average'].replace('[\$,]', '', regex=True).astype(long)
+df['ave_total'] = df['Total Box Office'].replace('[\$,]', '', regex=True).astype(long)
 
+print('\n\n\n############## BeautifulSoup Approach ##############')
+print(df.head(5))
+print(df.info())
+print(df.columns.values) 
+print(df.shape)
+print(df.describe())
 
+#####################
+## Pandas Approach ##
+#####################
 
+# use read_html to get a list of dataframes of the tables in the HTML
+dfs = pd.read_html(url, flavor='bs4')
 
-for item in s.find_all('a'):
-  print item.text
+# extract the one and only dataframe
+df = dfs[0]
 
-for item in s.find_all('tr'):
-  print item.text
+# add new columns in numeric data types
+df['ave_num'] = df['Average'].replace('[\$,]', '', regex=True).astype(long)
+df['ave_total'] = df['Total Box Office'].replace('[\$,]', '', regex=True).astype(long)
+
+print('\n\n\n############## Pandas Approach ##############')
+print(df.head(5))
+print(df.info())
+print(df.columns.values) 
+print(df.shape)
+print(df.describe())
