80fb3de6f499ed0905c6636eaad073cdacf19bed
Jonathan Halverson <halverson.jonathan@gmail.com>
2017-02-13 16:30:18 -0500

    Script that just does PPV

--- misc/mma_analytics/scrape_cards.py
+++ misc/mma_analytics/scrape_cards.py
@@ -6,6 +6,11 @@
 # flag to get updated data or not
 scrape_cards = False
 
+
+
+https://en.wikipedia.org/wiki/List_of_UFC_events
+
+
 # create a list of the card numbers
 cards = range(1, 209)
 cards.reverse()
@@ -23,6 +28,8 @@ if scrape_cards:
 
 import numpy as np
 import pandas as pd
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.width', 200)
 
 # store the table index for each card
 table_idx = dict([(card, 2) for card in cards])
@@ -43,34 +50,41 @@ for card in cards:
     html = f.read()
   # extract table
   dfs = pd.read_html(html)
-  df = dfs[table_idx[card]]
+  df = dfs[table_idx[card]].copy()
   # drop rows and concatenate
   df = df[df[2].isin(['def.', 'vs.', 'def', 'vs'])]
   # load table of fight card meta data
-  print card, [(i, dfs[i].shape) for i in range(len(dfs))]
+  #print card, [(i, dfs[i].shape) for i in range(len(dfs))]
   venue = dfs[info_idx[card]]
   venue.set_index(0, inplace=True)
   # add new columns
+  df['Card'] = 'UFC ' + str(card)
   df['Location'] = venue.loc['City', 1]
   df['Date'] = venue.loc['Date', 1]
   fights = pd.concat([fights, df], ignore_index=True)
 
 fights.reset_index(drop=True, inplace=True)
 fights.drop(labels=[7], axis=1, inplace=True)
-fights.columns = ['Weight', 'Winner', 'DefVs', 'Loser', 'Method', 'Round', 'Time', 'Location', 'Date'] 
+fights.columns = ['Weight', 'Winner', 'DefVs', 'Loser', 'Method', 'Round', 'Time', 'Card', 'Location', 'Date'] 
 
-fights['Winner'] = fights['Winner'].str.replace('\(c\)|\(ic\)|\(UFC Champion\)', repl='').str.strip()
-fights['Loser'] = fights['Loser'].str.replace('\(c\)|\(ic\)|\(Pride Champion\)', repl='').str.strip()
+# clean names
+fights['Winner'] = fights['Winner'].str.replace('\\(c\\)|\\(ic\\)|\\(UFC Champion\\)', repl='').str.strip()
+fights['Loser'] = fights['Loser'].str.replace('\\(c\\)|\\(ic\\)|\\(Pride Champion\\)', repl='').str.strip()
+
+# clean dates
 fights.Date = fights.Date.str.encode('ascii', errors='ignore')
+fights.Date = fights.Date.apply(lambda x: x if '(' not in x else x[x.index('(') + 1:-1])
+fights.Date = fights.Date.str.replace('\\[.\\]', '')
+fights.Date = pd.to_datetime(fights.Date)
 
-fighter = 'Conor McGregor'
 fighter = u'José Aldo'
 fighter = 'Nick Diaz'
 fighter = 'Carlos Condit'
-fighter = 'Georges St-Pierre'
 fighter = u'Patrick Côté'
-fighter = 'Anderson Silva'
+fighter = 'Georges St-Pierre'
 fighter = 'Rafael dos Anjos'
+fighter = 'Conor McGregor'
+fighter = 'Anderson Silva'
 
 #print fights[(fights.Winner.str.contains('Georges')) | (fights.Loser.str.contains('Georges'))]
 #print fights[(fights.Winner == fighter) | (fights.Loser == fighter)]
@@ -78,4 +92,11 @@ fighter = 'Rafael dos Anjos'
 #print fights[(~fights.Method.str.contains('TKO')) & (fights.Method.str.contains('KO'))]
 #print fights[fights.Method.str.contains('no contest', case=False)]
 #print fights.Winner.apply(lambda x: x if len(x.split()) < 2 else 'NaN').unique()
-print fights.Date.unique()
+#print fights.Date.apply(lambda x: x.year).value_counts().sort_index()
+
+#fights['DayOfWeek'] = fights.Date.dt.weekday_name
+#print fights[['Card', 'Location', 'Date', 'DayOfWeek']].drop_duplicates().DayOfWeek.value_counts()
+
+# Submissions vs KOs in championship rounds
+print fights[(fights.Round.isin(['4', '5'])) & (fights.Method.str.contains('Submission', case=False))]
+print fights[(fights.Round.isin(['4', '5'])) & (fights.Method.str.contains('KO', case=False))]
