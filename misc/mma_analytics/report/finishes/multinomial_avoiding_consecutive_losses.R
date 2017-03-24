c33a147d8df39308dc44743bd251b43a2af9b23c
Jonathan Halverson <halverson.jonathan@gmail.com>
2017-03-23 22:26:19 -0400

    R code to compute CDF of multinomial distribution

---
+++ misc/mma_analytics/report/finishes/multinomial_avoiding_consecutive_losses.R
@@ -0,0 +1,32 @@
+# Jonathan Halverson
+# Thursday, March 23, 2017
+# Evaulation of the CDF of the multinomial distribution
+
+p_draw = 27 / 4068.0
+p_win = 0.5 - 0.5 * p_draw
+p_lose = 0.5 - 0.5 * p_draw
+pbs = c(p_win, p_lose, p_draw)
+sum(pbs)
+total = 2487
+total = 3264
+
+ct = 0
+cdf = 0.0
+thres = dmultinom(x=c(1249, 1226, 12), prob=pbs)
+thres = dmultinom(x=c(1699, 1545, 20), prob=pbs)
+thres = 10
+for (i in seq(0, total)) {
+  for (k in seq(0, total)) {
+    if (i + k <= total) {
+      ct = ct + 1
+      y = c(i, total - i - k, k)
+      prb = dmultinom(x=y, size=total, prob=pbs)
+      if (prb <= thres) {
+        cdf = cdf + prb
+        #print(y)
+      } 
+    }
+  }
+}
+cdf
+ct
